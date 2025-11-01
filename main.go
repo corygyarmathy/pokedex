1ee254fc61353f345ab1a0233f5bc92572372c72
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-01 10:22:30 +0800

    feat: create main REPL function with help and exit commands

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func main() {
+	stdin := os.Stdin
+	scanner := bufio.NewScanner(stdin)
+	commands := getCommands()
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintf(os.Stderr, "scan error: %v\n", err)
+			}
+			break // EOF or error: exit REPL
+		}
+		line := scanner.Text()
+		fields := cleanInput(line)
+		if len(fields) == 0 {
+			continue
+		}
+		command := fields[0]
+
+		if c, exists := commands[command]; exists {
+			err := c.callback()
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+
+		}
+	}
+
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	commands := getCommands()
+	for _, c := range commands {
+		fmt.Printf("%v: %v\n", c.name, c.description)
+	}
+	return nil
+}
