c1c33859607a59c429d3678878c7426b77a6820b
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-02 15:14:11 +0800

    feat: add commandMapFwrd() and commandMapBack() commands, fetching location areas

---
+++ command_map.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandMapFwrd(cfg *config, args []string) error {
+	result, err := cfg.pokeAPIClient.FetchLocationAreas(cfg.nextLocURL)
+	if err != nil {
+		return fmt.Errorf("fetching locations: %w", err)
+	}
+
+	cfg.nextLocURL = result.Next
+	cfg.prevLocURL = result.Previous
+
+	for _, location := range result.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
+
+func commandMapBack(cfg *config, args []string) error {
+	if cfg.prevLocURL == nil {
+		fmt.Println("You're on the first page")
+		return nil
+	}
+
+	result, err := cfg.pokeAPIClient.FetchLocationAreas(cfg.prevLocURL)
+	if err != nil {
+		return fmt.Errorf("fetching previous locations: %w", err)
+	}
+
+	cfg.nextLocURL = result.Next
+	cfg.prevLocURL = result.Previous
+
+	for _, location := range result.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
